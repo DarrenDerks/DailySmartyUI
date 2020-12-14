7425f318ff7956e0291c82dc121d41ead64bd2a9
Darren Derks <leederksjr@gmail.com>
2020-12-14 05:17:42 -0700

    modified post component to render a result post and setup render links

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -14,7 +14,7 @@ class RecentPosts extends Component {
   renderPosts = function () {
     const posts = this.props.recentPosts.map((post, index) => {
       if (index < 3) {
-        return <Post {...post} key={index} />;
+        return <Post type="recent" {...post} key={index} />;
       }
     });
     return posts;
@@ -23,9 +23,9 @@ class RecentPosts extends Component {
   render() {
     return (
       <div className="recent-posts">
-        <div className="recent-posts_wrapper">
-          <div className="recent-posts_heading">Recent Posts</div>
-          <ul className="recent-posts_posts">{this.renderPosts()}</ul>
+        <div className="recent-posts__wrapper">
+          <div className="recent-posts__heading">Recent Posts</div>
+          <ul className="recent-posts__posts">{this.renderPosts()}</ul>
         </div>
       </div>
     );
